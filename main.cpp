4a5ee477d15f1e9ec8d03268dcad69648c6cd1a1
Liam <yamyamm1424@gmail.com>
2025-03-22 08:56:00 +0800

    tweaked user controls

--- main.cpp
+++ main.cpp
@@ -30,10 +30,10 @@ Apple apple = { 0 };
 int snakeLength = DEFAULT_SNAKE_INIT_LENGTH;
 
 static void InitGame(void);
-static void GrowSnake(void);
 static void UpdateGame(void);
 static void CollisionWall(void);
 static void CollisionSelf(void);
+static void CollisionApple(void);
 static void DrawElements(void);
 
 int main(void)
@@ -70,39 +70,26 @@ void InitGame() {
     apple.size = { (float)SQUARE_SIZE, (float)SQUARE_SIZE };
     apple.color = YELLOW;
 }
-
-void GrowSnake() {
-	snake[snakeLength].position = snake[snakeLength - 1].position;
-	snake[snakeLength].size = snake[snakeLength - 1].size;
-	snake[snakeLength].speed = snake[snakeLength - 1].speed;
-	snake[snakeLength].color = snake[snakeLength - 1].color;
-	snakeLength++;
-}
-
 void UpdateGame() {
-    if (IsKeyPressed(KEY_LEFT))
+    if (IsKeyPressed(KEY_LEFT) && snake[0].speed.x != (float)SQUARE_SIZE)
     {
         snake[0].speed.x = (float)-SQUARE_SIZE; //If left key, move left (negative x)
         snake[0].speed.y = 0; //No y movement
     }
-    if (IsKeyPressed(KEY_UP))
+    if (IsKeyPressed(KEY_UP) && snake[0].speed.y != (float)SQUARE_SIZE)
     {
         snake[0].speed.x = 0; //No x movement
         snake[0].speed.y = (float)-SQUARE_SIZE; //If up key, move up (negative y)
     }
-    if (IsKeyPressed(KEY_DOWN))
+    if (IsKeyPressed(KEY_DOWN) && snake[0].speed.y != (float)-SQUARE_SIZE)
     {
         snake[0].speed.x = 0; //No x movement
         snake[0].speed.y = (float)SQUARE_SIZE; //If down key, move down (positive y)
     }
-    if (IsKeyPressed(KEY_RIGHT))
+    if (IsKeyPressed(KEY_RIGHT) && snake[0].speed.x != (float)-SQUARE_SIZE)
     {
         snake[0].speed.x = (float)SQUARE_SIZE; //If right key, move right (positive x)
         snake[0].speed.y = 0; //No y movement
-    }
-
-    if (IsKeyDown(KEY_SPACE)) {
-        GrowSnake();
     }
 
     // Makes sure that it remembers where the head is now
@@ -119,11 +106,12 @@ void UpdateGame() {
             snake[i].position = previousPosition; // Moves it to where the part in front was
             previousPosition = temp; // Update the previous position for the next part
         }
-        if (snake[0].position.x == apple.position.x && snake[0].position.y == apple.position.y) {
-            GrowSnake(); 
-        }
+        
     }
 	frameCounter++;
+	CollisionWall();
+	CollisionSelf();
+    CollisionApple();
 }
 
 void DrawElements(void) {
@@ -152,15 +140,31 @@ void DrawElements(void) {
 void CollisionWall(void) {
     if (snake[0].position.x >= SCREEN_WIDTH || snake[0].position.x < 0) {
         std::cout << "Game Over!" << std::endl;
+		exit(0);
     }
     if (snake[0].position.y >= SCREEN_HEIGHT || snake[0].position.y < 0) {
         std::cout << "Game Over!" << std::endl;
+        exit(0);
     }
 }
 void CollisionSelf(void) {
         for (int i = 1; i < snakeLength; i++) {
             if ((snake[0].position.x == snake[i].position.x) && (snake[0].position.y == snake[i].position.y)) {
                 std::cout << "Game Over!" << std::endl;
+                exit(0);
             }
         }
     }
+
+void CollisionApple(void) {
+    if (frameCounter % 5 == 0) {
+        if (snake[0].position.x == apple.position.x && snake[0].position.y == apple.position.y) {
+            snake[snakeLength].position = snake[snakeLength - 1].position;
+            snake[snakeLength].size = snake[snakeLength - 1].size;
+            snake[snakeLength].speed = snake[snakeLength - 1].speed;
+            snake[snakeLength].color = snake[snakeLength - 1].color;
+            snakeLength++;
+			std::cout << "Apple eaten!" << std::endl;
+        }
+    }
+}
